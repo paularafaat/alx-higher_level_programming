3181a2e4d8ed8907887204312caa19eb1e530b37
paula Raafat <paularaafat27@gmail.com>
2024-02-05 15:27:11 +0200

    roman string

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+    roman_nums = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    x = 0
+    for char in roman_string:
+        for key, value in roman_nums.items():
+            if char == key:
+                x += value
+    return x

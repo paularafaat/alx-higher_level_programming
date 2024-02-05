04f3ba4855e7dc1f69e076f3e76e7e1afdd112a1
paula Raafat <paularaafat27@gmail.com>
2024-02-05 14:00:05 +0200

    dit & set

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     new_dic = {}
     for key in a_dictionary:
-        new_dic[key] =a_dictionary[key] * 2
+        new_dic[key] = a_dictionary[key] * 2
     return new_dic

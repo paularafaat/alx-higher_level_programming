c971fa8458fbd3970ef4131cf5351b4b05158fd1
paula Raafat <paularaafat27@gmail.com>
2024-02-15 13:58:32 +0200

    safe print lsit

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    i = 0
-    try:
-        for i in range(x):
-            if type(my_list[i]) == int:
-                print("{:d}".format(my_list[i]), end='')
-                i += 1
-    except (ValueError, TypeError, IndexError):
-        None
+    if my_list is None:
+        my_list = []
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+            count += 1
+        except (ValueError, TypeError):
+            pass
     print()
-    return i
+    return count

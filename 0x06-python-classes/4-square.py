0e805e1642a3020608b33807f788a32b34617cec
paula Raafat <paularaafat27@gmail.com>
2024-03-10 22:17:00 +0200

    empty class

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""square module"""
+
+
+class Square:
+    """Size validation"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    """getter func"""
+    def size(self):
+        self.__size = size
+
+    @size.setter
+    """setter func"""
+    def size(self, value):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    """square area function"""
+    def area(self):
+        return self.__size ** 2

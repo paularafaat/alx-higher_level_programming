262bd9764f8331316bef7ac5bcaa6581d3102ea0
paula raafat <paularaafat27@gmail.com>
2024-04-10 19:33:38 +0200

    add integer

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -9,9 +9,11 @@ def say_my_name(first_name, last_name=""):
         first_name (str): first name
         last_name (str, none): last name. Defaults to "".
     """
-    if not isinstance(first_name, str) or not isinstance(last_name, str):
-        raise TypeError("first_name must be a string or \
-last_name must be a string")
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
     print("My name is " + first_name + " " + last_name)
 
 

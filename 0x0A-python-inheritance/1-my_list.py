24103bee930769579bd5055c05d25be441aebd5f
paula raafat <paularaafat27@gmail.com>
2024-04-16 01:23:21 +0200

    print sorted list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""My list class"""
+
+class Mylist(list):
+    """My list class"""
+
+    def print_sorted(self):
+        """printing sorted list"""
+        print(sorted(self))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/1-my_list.txt")
+

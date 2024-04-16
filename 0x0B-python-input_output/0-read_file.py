5df115742c52387f44294994161b2b712de3ada2
paula raafat <paularaafat27@gmail.com>
2024-04-17 01:57:43 +0200

    handling file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """reading a file"""
     with open(filename, "r", encoding="utf-8") as f:
-        for line in f:
-            print(line)
+        content = f.read()
+        print(content)

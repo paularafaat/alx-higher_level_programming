5ddf58a953ba875d792f9dc890d5db7edca129a2
paula raafat <paularaafat27@gmail.com>
2024-04-16 02:35:11 +0200

    inhertance

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""base geometry module"""
+
+
+class BaseGeometry:
+    """empty class"""
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""BaseGeometry class"""
+
+
+class BaseGeometry:
+    """BaseGeometry"""
+    def area(self):
+        """raises  an Exception """
+        raise Exception("area() is not implemented")

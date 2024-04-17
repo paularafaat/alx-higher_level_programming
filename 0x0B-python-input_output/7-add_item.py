c539a05e387b0bb4d59cdca2f56ebfdb5e74bf55
paula raafat <paularaafat27@gmail.com>
2024-04-17 03:08:38 +0200

    handling file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
+"""add item module"""
+
+
+import sys
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-import sys
-
-
 try:
-    n_list = load_from_json_file("add_item.json")
+    o_list = load_from_json_file("add_item.json")
 except FileNotFoundError:
-    n_list = []
+    o_list = []
 
-n_list.extend(sys.argv[1:])
-save_to_json_file(n_list, "add_item.json")
+o_list.extend(sys.argv[1:])
+save_to_json_file(o_list, "add_item.json")

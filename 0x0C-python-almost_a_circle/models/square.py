150bada7c9eb283563f1be58aad9152609dc18ed
paula raafat <paularaafat27@gmail.com>
2024-04-23 00:12:05 +0200

    almost_a_circle project

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,3 +12,15 @@ class Square(Rectangle):
     def __str__(self):
         """Return string representation of Square"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        """Getter  for size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for size"""
+        self.validate_attribute("width", value)
+        self.width = value
+        self.height = value

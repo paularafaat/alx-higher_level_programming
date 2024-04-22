e2f411472e240b9e58a40faea2af5b2881b26129
paula raafat <paularaafat27@gmail.com>
2024-04-23 00:53:23 +0200

    almost_a_circle project

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,10 +14,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
     @staticmethod
     def to_json_string(list_dictionaries):
         """Return JSON string representation of list_dictionaries"""
-        if list_dictionaries is None or not list_dictionaries:
+        if list_dictionaries is None:
             return "[]"
         else:
             return json.dumps(list_dictionaries)

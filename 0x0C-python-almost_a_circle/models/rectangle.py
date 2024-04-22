422a239e383e175f73d8ad2d101ba94f2e2e338c
paula raafat <paularaafat27@gmail.com>
2024-04-22 22:55:18 +0200

    almost_a_circle project

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -84,3 +84,12 @@ class Rectangle(Base):
         """return string representation of Rectangle"""
         return (f"[Rectangle] ({self.id}) {self.x}/{self.y} - "
                 f"{self.width}/{self.height}")
+
+    def update(self, *args):
+        """update rectangle attributes with positional arguments"""
+
+        self.id = args[0]
+        self.width = args[1]
+        self.height = args[2]
+        self.x = args[3]
+        self.y = args[4]

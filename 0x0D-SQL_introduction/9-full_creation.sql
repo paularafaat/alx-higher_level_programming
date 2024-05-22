fb73a221be1bfad6482c8c08abda57cfc4906b20
paula raafat <paularaafat27@.com>
2024-05-22 04:37:46 +0300

    SQL intro

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,10 +1,8 @@
 -- creates a table second_table in the database hbtn_0c_0
-CREATE table IF NOT EXISTS second_table ('id' INT,
-'name' VARCHAR(256),
-'score' INT);
+CREATE table IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score)
 VALUES
     (1, 'John', 10),
     (2, 'Alex', 3),
-    (3, 'BOB', 14),
+    (3, 'Bob', 14),
     (4, 'George', 8);

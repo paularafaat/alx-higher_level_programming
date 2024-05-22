995108ab0151c6ec4b4fb652c3811eafcfa4ef68
paula raafat <paularaafat27@.com>
2024-05-22 03:58:17 +0300

    SQL intro

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
--- prints the full description of the table first_table from the database hbtn_0c_0
+-- prints the full description of the table first_table
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA, COLUMN_COMMENT
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';

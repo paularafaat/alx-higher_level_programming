811e5b218485e2d6b5e35305777a2402b40abcb2
paula raafat <paularaafat27@.com>
2024-06-19 02:17:16 +0300

    JS

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 function add(a, b) {
-    a = parseInt(a);
-    b = parseInt(b);
+    a = parseInt(process.argv[2]);
+    b = parseInt(process.argv[3]);
     if (isNaN(a) || isNaN(b)) {
       console.log("NaN");
     } else {

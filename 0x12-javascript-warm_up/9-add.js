0172442bb4f1c97d2969161ba02b4b233fa12ccd
paula raafat <paularaafat27@.com>
2024-06-19 02:19:28 +0300

    JS

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,13 +1,5 @@
 #!/usr/bin/node
-
-function add(a, b) {
-    a = parseInt(process.argv[2]);
-    b = parseInt(process.argv[3]);
-    if (isNaN(a) || isNaN(b)) {
-      console.log("NaN");
-    } else {
-      const sum = a + b;
-      console.log(sum);
-    }
-  }
-  
+function add (a, b) {
+  const sum = a + b;
+  console.log(sum);
+}

5ab386f3bdcda6ad93b8af54e9eb26701dcb57b3
paula raafat <paularaafat27@.com>
2024-06-19 23:33:03 +0300

    JS

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (typeof w !== 'number' || typeof h !== 'number' || w <= 0 || h <= 0) {
-      return {};
+    if (w <= 0 || h <= 0) {
+      return;
     }
     this.width = w;
     this.height = h;

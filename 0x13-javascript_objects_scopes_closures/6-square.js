ce79344788e2ed99d1e1ae299b7850f8c95850b0
paula raafat <paularaafat27@.com>
2024-06-20 02:21:45 +0300

    Js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,12 +2,13 @@
 const BaseSquare = require('./5-square');
 
 class Square extends BaseSquare {
+
   charPrint (c) {
     if (c !== 'C' || c === undefined) {
       c = 'X';
     }
-    for (let i = 0; i < this.size; i++) {
-      console.log(c.repeat(this.size));
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.width));
     }
   }
 }

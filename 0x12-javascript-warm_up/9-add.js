6b61f1d91c987111b5f3cc9de46638f6bd2cc127
paula raafat <paularaafat27@.com>
2024-06-19 03:11:16 +0300

    JS

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 function add (a, b) {
-  console.log(parseInt(process.argv[2]), parseInt(process.argv[3]));
+  console.log(parseInt(process.argv[2]) + parseInt(process.argv[3]));
 }

bab4a42e75fce191082af4edbb6e0358354f63e8
paula raafat <paularaafat27@.com>
2024-06-23 22:48:17 +0300

    ORM

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" lists all states matches the searched """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port= 3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
+    searched_name = sys.argv[4]
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC", ('%' + searched_name + '%',))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()

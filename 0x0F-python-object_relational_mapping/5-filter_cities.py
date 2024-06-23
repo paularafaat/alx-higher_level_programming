b3c7e534150f922356d579e797b19d3848ee9b6f
paula raafat <paularaafat27@.com>
2024-06-24 01:15:53 +0300

    ORM

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" lists all cities """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
+    cur = conn.cursor()
+    state_name = sys.argv[4]
+    cur.execute("""SELECT cities.name FROM
+                cities JOIN states ON cities.state_id = states.id
+                WHERE states.name = %s""", (state_name,))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row[0])
+    cur.close()
+    conn.close()

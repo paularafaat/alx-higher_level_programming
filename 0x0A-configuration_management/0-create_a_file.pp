6dc901cd6fb71a683160d91a91aa82fc4d91b5f7
paula raafat <paularaafat27@.com>
2024-06-29 02:43:56 +0300

    puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # This Puppet manifest ensures the creation of a file with specific attributes.
 file { '/tmp/school':
-    ensures => 'file',
-    content => 'I love Puppet'
-    mode    => '0744'
-    owner   => 'www-data'
-    group   => 'www-data'
+    ensure  => 'file',
+    content => 'I love Puppet',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
 }

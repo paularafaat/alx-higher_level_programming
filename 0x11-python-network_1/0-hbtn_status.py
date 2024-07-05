d95c04e2a9826ca97aaa2a3977c1197d61463d6a
paula raafat <paularaafat27@.com>
2024-07-06 00:14:56 +0300

    network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """ status module """
 import urllib.request
-url = 'https://alx-intranet.hbtn.io/status'
-with urllib.request.urlopen(url) as res:
-    con = res.read()
 
 
-print(f"\t- type: {type(con)}")
-print(f"\t- content: {con}")
-print(f"\t- utf8: {con.endcode('utf-8')}")
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as res:
+        con = res.read()
+    print(f"\t- type: {type(con)}")
+    print(f"\t- content: {con}")
+    print(f"\t- utf8: {con.endcode('utf-8')}")

01a1719cb5845dd5b1b5f823c2e039b53d664237
paula raafat <paularaafat27@.com>
2024-07-06 03:38:03 +0300

    network

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""script that fetches https://alx-intranet.hbtn.io/status"""
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print(f"\t- type: {type(r)}")
+    print(f"\t- content: {r}")

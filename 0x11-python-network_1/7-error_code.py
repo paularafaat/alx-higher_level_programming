dd51d7f9d226d4af35af88a4a47edf15e3f79487
paula raafat <paularaafat27@.com>
2024-07-06 03:51:26 +0300

    network

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Send a POST request with an email as a parameter and display the response"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+
+if response.status_code >= 400:
+    print(f"Error code: {response.status_code}")
+else:
+    print(response.text)
